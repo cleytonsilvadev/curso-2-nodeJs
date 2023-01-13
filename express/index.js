56f614cd4538eac92c25d56657dea27ea8db23cc
Cleyton Rodrigues <88549697+cleytonsilvadev@users.noreply.github.com>
2023-01-13 19:26:21 -0300

    13/01

--- express/index.js
+++ express/index.js
@@ -10,13 +10,43 @@ app.get("/",function(req,res){
 
 app.get("/blog",function(req,res){
     res.send("bem vindo ao meu blog");
-})
+});
 
 app.get("/canal/youtube",function(req,res){
     res.send("<h1>bem vindo ao meu teste</h1>");
-})
+});
+    
+app.get("/ola/:nome",function(req,res){
+    var nome=req.params.nome;
+    res.send("<h1>oi " + nome+"<h1>");
+});
+    //parametros obgatorios
+app.get("/ola/:nome/:empresa",function(req,res){
+    var nome=req.params.nome;
+    var empresa=req.params.empresa;
+    res.send("<h1>oi " + nome+" do "+empresa+"<h1>");
+});
+    //parametros opcionais
+    /*
+app.get("/blog/:artigo?",function(req,res){
+    var artigo=req.params.artigo;
 
+    if(artigo){
+        res.send("<h2>artigo:"+artigo+"</h2>")    
+    }else{
+        res.send("bem vindo ao meu blog "+artigo);
+}
+});
+*/
+app.get("/blog/:blog?",function(req,res){
+    var blog=req.query["blog"];
 
+    if(blog){
+        res.send(blog);   
+    }else{
+        res.send("nenhum canal fornecido");
+    }
+});
 
 app.listen(4000,function(erro){
     if(erro){
